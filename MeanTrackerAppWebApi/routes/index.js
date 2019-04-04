2301708f0131e2b24e09207a9edff8a1315e0068
Anas Peer <M1030444@mindtree.com>
2019-04-04 12:59:15 +0530

    Added the WEB API Base Line Solution

---
+++ MeanTrackerAppWebApi/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'This is the WEB API for Activity Tracker App' });
+});
+
+module.exports = router;
