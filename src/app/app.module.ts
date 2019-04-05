0666d17975ea38adcb124dc9099459b96d3f9863
Balaji Gopal <M1034617@mindtree.com>
2019-04-05 19:52:56 +0530

    Material Datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TimesheetAddComponent } from './timesheet-add/timesheet-add.component';
 import { TimesheetEditComponent } from './timesheet-edit/timesheet-edit.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,16 @@ import { TimesheetEditComponent } from './timesheet-edit/timesheet-edit.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [MatDatepickerModule, MatNativeDateModule ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/timesheet-add/timesheet-add.component.ts
+++ src/app/timesheet-add/timesheet-add.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-timesheet-add',
   templateUrl: './timesheet-add.component.html',
   styleUrls: ['./timesheet-add.component.css']
 })
-export class TimesheetAddComponent implements OnInit {
 
+export class TimesheetAddComponent implements OnInit {
+  startDate = new Date(1990, 0, 1);
+  date = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString())
   constructor() { }
 
   ngOnInit() {
