d57489e6f7005f2b957012f1750597c4da5ef7c4
Prmela Durairaj <M1034157@mindtree.com>
2019-04-11 11:44:07 +0530

    resolved router error

--- MeanTrackerAppWebApi/app.js
+++ MeanTrackerAppWebApi/app.js
@@ -20,11 +20,10 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
 );
-
-app.use(passport.initialize());
-app.use('/MeanTrackerAppWebApi', routesApi);
-
 var app = express();
+app.use(passport.initialize());
+app.use('/MeanTrackerAppWebApi', indexRouter);
+
 require("dotenv").config();
 
 // view engine setup
